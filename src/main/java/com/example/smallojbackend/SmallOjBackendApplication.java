c15f3ca29a83ee2e153e9881e76083139121067b
deltaLRD <ruidongli2002@gmail.com>
2023-09-02 21:47:14 +0800

    init basic framework

---
+++ src/main/java/com/example/smallojbackend/SmallOjBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.smallojbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallOjBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallOjBackendApplication.class, args);
+    }
+
+}
