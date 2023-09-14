d3a0d58b0323b8edf4a545d9a1040f5f218235c9
deltaLRD <ruidongli2002@gmail.com>
2023-09-14 15:16:25 +0800

    config redis

---
+++ src/main/java/com/example/smallojbackend/utils/Token.java
@@ -0,0 +1,25 @@
+package com.example.smallojbackend.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.security.interfaces.RSAKey;
+import java.util.Date;
+
+public class Token {
+    private final String key = "SomeRandomChars";
+    private final long ExpireTime = 15 * 60 * 1000;
+    public String createToken(String userId) {
+        try {
+            Date date = new Date(System.currentTimeMillis() + ExpireTime);
+            Algorithm algorithm = Algorithm.HMAC256(key);
+            return JWT.create()
+                    .withClaim("uid", userId)
+                    .withExpiresAt(date)
+                    .sign(algorithm);
+        }catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
